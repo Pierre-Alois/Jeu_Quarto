baf451fc7b091d50edcf7feb3e311c6f3167d3f7
PA062 <PA062@172.17.12.221>
2024-02-16 12:18:09 +0100

    13:02:24 Avant vac

---
+++ src/quarto/Ordi.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quarto;
+
+/**
+ *
+ * @author PA062
+ */
+public class Ordi extends Joueur {
+    
+    public Ordi(String pseudo) {
+        super(pseudo);
+    }
+    
+}
